eb0a91437cc99affb8fbac170e9cd88dbf9d5874
mhmadamrii <interceptorghost4@gmail.com>
2023-06-04 17:03:25 +0700

    special commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<nav>
+    <a href="/">Home page</a>
+    <a href="/about">About page</a>
+</nav>
+
+<slot></slot>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { onMount } from "svelte"
+  let posts = []
+
+  async function fetchPosts() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+    console.log(res)
+    posts = await res.json()
+  }
+
+  onMount(() => {
+    fetchPosts()
+  })
+</script>
+
+<main>
+  <center>
+    <h1>The posts may you like..</h1>
+    {#each posts as post }
+      <h1>{post.title}</h1>
+    {/each}
+  </center>
+</main>
