81f6c62b9679d315aefc93b78db6a944350e5150
mhmadamrii <interceptorghost4@gmail.com>
2023-07-08 18:33:52 +0700

    adding image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
+<script>
+  import img from "$lib/tes.png";
+</script>
+
 <div class="card w-96 bg-base-100 shadow-xl">
-  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <figure><img src={img} alt="Shoes" /></figure>
   <div class="card-body">
     <h2 class="card-title">Shoes!</h2>
     <p>If a dog chews shoes whose shoes does he choose?</p>
